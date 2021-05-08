5cb910628da51e86353c3c00e4a33b3ab04d55c4
Andrew DalPino <me@andrewdalpino.com>
2021-05-08 17:17:30 -0500

    No more predict sample

--- predict.php
+++ predict.php
@@ -4,6 +4,7 @@ include __DIR__ . '/vendor/autoload.php';
 
 use Rubix\ML\PersistentModel;
 use Rubix\ML\Persisters\Filesystem;
+use Rubix\ML\Datasets\Unlabeled;
 
 ini_set('memory_limit', '-1');
 
@@ -11,6 +12,10 @@ $estimator = PersistentModel::load(new Filesystem('sentiment.rbx'));
 
 while (empty($text)) $text = readline("Enter some text to analyze:\n");
 
-$prediction = $estimator->predictSample([$text]);
+$dataset = new Unlabeled([
+    [$text],
+]);
+
+$prediction = current($estimator->predict($dataset));
 
 echo "The sentiment is: $prediction" . PHP_EOL;
